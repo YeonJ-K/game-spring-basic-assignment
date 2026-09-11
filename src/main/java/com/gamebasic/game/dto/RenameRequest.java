548ccd2d533c1d82d58df58db92a83afc809da95
YeonJ-K <yeonj.0327@gmail.com>
2026-09-11 19:22:39 +0900

    Player Name Remove, Save Data Delete

--- src/main/java/com/gamebasic/game/dto/RenameRequest.java
+++ src/main/java/com/gamebasic/game/dto/RenameRequest.java
@@ -1,4 +1,14 @@
 package com.gamebasic.game.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class RenameRequest {
+    @NotBlank
+    @Size(min = 2, max = 12)
+    private String playerName;
+
+    public String getPlayerName() {
+        return playerName;
+    }
 }
