129f797eb392612b54c3110892656b5edc19873f
yeonJ <59119195+YeonJ-K@users.noreply.github.com>
2026-09-11 19:42:00 +0900

    Update README.d

--- README.d
+++ README.d
@@ -1,3 +1,35 @@
 # 붉은 달의 성채 REST API 명세서
 
   
+#ERD
+
+has
+GAME
+Long
+id
+PK
+String
+playerName
+int
+currentFloor
+int
+currentHp
+String
+phase
+String
+status
+LocalDateTime
+createdAt
+LocalDateTime
+updatedAt
+RUN_CARD
+Long
+id
+PK
+Long
+game_id
+FK
+String
+cardType
+int
+acquiredFloor
